84011b0f972c15b35a3caf265c590f923c6fcbb3
Yusuf <yusufisxq@gmail.com>
2024-09-11 08:19:37 +0300

    creating automated tests for formatCurrency

---
+++ tests/tests.js
@@ -0,0 +1,23 @@
+import { formatCurrency } from "../scripts/utily/money.js";
+console.log("Test suite: formatCurrency")
+console.log("rounding to the nearest cents")
+if (formatCurrency(2000.4) === "20.00") {
+    console.log("passed")
+} else {
+    console.log("failed")
+};
+
+console.log("converts cents to doller")
+if (formatCurrency(2095) === "20.95") {
+    console.log("passed")
+} else {
+    console.log("failed")
+};
+
+console.log("working with 0")
+if (formatCurrency(0) === "0.00") {
+    console.log("passed")
+} else {
+    console.log("failed")
+};
+
