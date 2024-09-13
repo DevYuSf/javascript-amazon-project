06c9ec2e558b8822a7e5f8081d1638a36606d545
Yusuf <yusufisxq@gmail.com>
2024-09-13 08:10:02 +0300

    create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -25,6 +25,7 @@ export function renderOrderSummary(){
         // console.log(matchingProduct);
         cartSummary+=`
             <div class="cart-item-container 
+            js-cart-item-container
             js-cart-item-container-${matchingProduct.id}
             ">
                 <div class="delivery-date">
@@ -42,14 +43,18 @@ export function renderOrderSummary(){
                     <div class="product-price">
                         $${formatCurrency(matchingProduct.priceCents)}
                     </div>
-                    <div class="product-quantity">
+                    <div class="product-quantity
+                    js-product-quantity-${matchingProduct.id}
+                    ">
                         <span>
                         Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                         </span>
                         <span class="update-quantity-link link-primary">
                         Update
                         </span>
-                        <span class="delete-quantity-link js-delete-quantity-link link-primary" data-product-id = ${matchingProduct.id}>
+                        <span class="delete-quantity-link js-delete-quantity-link link-primary
+                        js-delete-link-${matchingProduct.id}
+                        " data-product-id = ${matchingProduct.id}>
                         Delete
                         </span>
                     </div>
@@ -77,7 +82,6 @@ export function renderOrderSummary(){
                     `.js-cart-item-container-${productId}`
                 );
                 product.remove();
-                updateCartQuantity();
                 renderPaymentSummary();
             })
         });
@@ -126,7 +130,7 @@ export function renderOrderSummary(){
             document.querySelector(".js-return-to-home-link").innerHTML=`${calculateCartQuantity()} items`
         
     };
-    updateCartQuantity();
+   
 
     document.querySelectorAll('.js-delivery-option')
         .forEach((element)=>{
